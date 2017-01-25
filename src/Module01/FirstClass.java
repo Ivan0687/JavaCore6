a606cdd8847d91fa2cbb3533e84a57d6eea54ab2
Vancho <ivpavboyko@gmail.com>
2017-01-25 17:46:18 +0200

    Home Work 1

---
+++ src/Module01/FirstClass.java
@@ -0,0 +1,10 @@
+package Module01;
+
+/**
+ * Created by root on 25.01.2017.
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("Мой первый класс на Java");
+    }
+}
---
+++ src/Module01/ThirdClass.java
@@ -0,0 +1,11 @@
+package Module01;
+
+/**
+ * Created by root on 25.01.2017.
+ */
+public class ThirdClass {
+    public static void main(String[] args) {
+        System.out.println("Мой первый класс на Java");
+}
+
+}
