0d8c0875064abebbd009a5bb69cc7bd1c6cecf77
Vancho <ivpavboyko@gmail.com>
2017-01-29 19:12:25 +0200

    module3, homework 3.1

---
+++ src/module03/Bird.java
@@ -0,0 +1,24 @@
+package module03;
+
+/**
+ * Created by root on 29.01.2017.
+ */
+public class Bird {
+
+    private static void sing (String[] lyrics){
+
+        for (String lyric : lyrics) {
+
+            System.out.println(lyric);
+
+        }
+    }
+
+    public static void main(String[] args) {
+
+        String[] song = new String[]{"I am walking", "I am flying", "I am singing", "I am Bird"};
+
+        Bird.sing(song);
+
+    }
+}
